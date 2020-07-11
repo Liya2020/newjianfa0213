e7ef03dd0d078834cae85a878b06b5f6f5de2f18
Liya2020 <2770651633@qq.com>
2020-07-11 13:15:56 +0800

    666

---
+++ src/main/java/com/atguigu/Dugujiujian.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+/**
+ * @author Liya
+ * @create 2020-07-11 13:06
+ */
+public class Dugujiujian {
+    public static void main(String[] args) {
+        System.out.println("AAAAAAA");
+        System.out.println("BBBBBBB");
+    }
+}
+
